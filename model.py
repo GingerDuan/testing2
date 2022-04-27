a474bcbc1858f2a9bd3d1de475d1dbc41e27b46f
Ginger Duan <gingerduanl@gmail.com>
2022-04-27 03:42:31 -0700

    2nd review and delete last time extra work

--- model.py
+++ model.py
@@ -11,15 +11,6 @@ class Game(db.Model):
     name = db.Column(db.String(20), nullable=False, unique=True)
     description = db.Column(db.String(100))
 
-class CandyLand(db.Model):
-    """Not a real Board game."""
-
-    __tablename__ = "Candy_Land"
-    game_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    name = db.Column(db.String(20), nullable=False, unique=True)
-    description = db.Column(db.String(100))
-
-
 def connect_to_db(app, db_uri="postgresql:///testdb"):
     app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
     db.app = app
@@ -28,7 +19,7 @@ def connect_to_db(app, db_uri="postgresql:///testdb"):
 
 def example_data():
     """Create example data for the test database."""
-    test_game = CandyLand(name = "candyland", description = "Pull a random card from the pile and the card will tell you how many spaces you can move up the board. If you get the 'cookie' card the game will end for you.", )
+    gmae = Game(name = "candyland", description = "Pull a random card from the pile and the card will tell you how many spaces you can move up the board. If you get the 'cookie' card the game will end for you.", )
 
 if __name__ == '__main__':
     from party import app
